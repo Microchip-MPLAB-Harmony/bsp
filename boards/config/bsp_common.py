9d415550601aef196ed605ba0ddcbf5cccb86584
Thibaut <http://support.microchip.com>
2018-03-13 20:03:03 +0100

    [config] Fixing MHC symbols

--- boards/config/bsp_common.py
+++ boards/config/bsp_common.py
@@ -1,5 +1,5 @@
 
-bspMenu = bspComponent.createMenuSymbol(None, None)
+bspMenu = bspComponent.createMenuSymbol("BSP_MENU", None)
 bspMenu.setLabel("BSP Pin Types")
 bspMenu.setDescription("Type of PIO Pins")
 
@@ -8,7 +8,7 @@ enumeratedPinTypes = enumerate(pinTypes)
 for enumeratedPinType in enumeratedPinTypes:
 	pinTypeIndex, pinType = enumeratedPinType
 
-	Menu = bspComponent.createMenuSymbol(None, bspMenu)
+	Menu = bspComponent.createMenuSymbol("BSP_SUBMENU", bspMenu)
 	Menu.setLabel("Type " + str(pinTypeIndex))
 
 	for pinAttribute in pinAttributes:
@@ -21,19 +21,19 @@ for enumeratedPinType in enumeratedPinTypes:
 Symbol = bspComponent.createIntegerSymbol("BSP_TYPE_SIZE", bspMenu)
 Symbol.setDefaultValue(len(pinTypes))
 
-bspSysConfFile = bspComponent.createFileSymbol(None, None)
+bspSysConfFile = bspComponent.createFileSymbol("BSP_SYSTEM_CONFIG_H", None)
 bspSysConfFile.setType("STRING")
 bspSysConfFile.setOutputName("core.LIST_SYSTEM_CONFIG_H_APPLICATION_CONFIGURATION")
 bspSysConfFile.setSourcePath("templates/system_config.h.ftl")
 bspSysConfFile.setMarkup(True)
 
-bspSysConfInclude = bspComponent.createListEntrySymbol(None, None)
+bspSysConfInclude = bspComponent.createListEntrySymbol("INCLUDE_BSP_H", None)
 bspSysConfInclude.setTarget("core.LIST_SYSTEM_CONFIG_H_GLOBAL_INCLUDES")
 bspSysConfInclude.addValue("#include \"bsp/bsp.h\"")
 
 configName = Variables.get("__CONFIGURATION_NAME")
 
-bspSourceFile = bspComponent.createFileSymbol(None, None)
+bspSourceFile = bspComponent.createFileSymbol("BSP_C", None)
 bspSourceFile.setSourcePath(BSP_NAME + "/templates/bsp.c.ftl")
 bspSourceFile.setOutputName("bsp.c")
 bspSourceFile.setMarkup(True)
@@ -42,7 +42,7 @@ bspSourceFile.setDestPath("bsp/")
 bspSourceFile.setProjectPath("config/" + configName + "/bsp/")
 bspSourceFile.setType("SOURCE")
 
-bspHeaderFile = bspComponent.createFileSymbol(None, None)
+bspHeaderFile = bspComponent.createFileSymbol("BSP_H", None)
 bspHeaderFile.setSourcePath(BSP_NAME + "/templates/bsp.h.ftl")
 bspHeaderFile.setOutputName("bsp.h")
 bspHeaderFile.setMarkup(True)
