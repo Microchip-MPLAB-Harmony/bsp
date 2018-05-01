d7581176ab237113ce4632554d9b9b52d282cf88
Settu Duraisamy <settu.duraisamy@microchip.com>
2018-05-01 22:40:08 +0530

    [BSP] Assigned LED for E70 XPLD and added V71 BSP support

--- boards/sam_e70_xpld/config/bsp.py
+++ boards/sam_e70_xpld/config/bsp.py
@@ -1,22 +1,40 @@
 def instantiateComponent(bspComponent):
 
-	BSP_NAME = "sam_e70_xpld"
+    # LED
+    Database.clearSymbolValue("core", "PIN_82_FUNCTION_NAME")
+    Database.clearSymbolValue("core", "PIN_82_FUNCTION_TYPE")
+    Database.clearSymbolValue("core", "PIN_82_LAT")
 
-	pinAttributes = [{"attrib":"type", "symbol":"BSP_CUSTOM_TYPE", "label":"Type Name"},
-		{"attrib":"mode", "symbol":"BSP_CUSTOM_MODE", "label":"Mode"},
-		{"attrib":"dir", "symbol":"BSP_CUSTOM_DIR", "label":"Direction"},
-		{"attrib":"lat", "symbol":"BSP_CUSTOM_LAT", "label":"Initial Latch Value"},
-		{"attrib":"od", "symbol":"BSP_CUSTOM_OD", "label":"Open Drain"},
-		{"attrib":"cn", "symbol":"BSP_CUSTOM_CN", "label":"Change Notice"},
-		{"attrib":"pu", "symbol":"BSP_CUSTOM_PU", "label":"Pull Up"},
-		{"attrib":"pd", "symbol":"BSP_CUSTOM_PD", "label":"Pull Down"},
-		{"attrib":"int", "symbol":"BSP_CUSTOM_PIO_INTERRUPT", "label":"PIO Interrupt"}]
+    Database.setSymbolValue("core", "PIN_82_FUNCTION_NAME", "LED")
+    Database.setSymbolValue("core", "PIN_82_FUNCTION_TYPE", "LED_AL")
+    Database.setSymbolValue("core", "PIN_82_LAT", "High")
+    
+   #Switch 1
+    Database.clearSymbolValue("core", "PIN_64_FUNCTION_NAME")
+    Database.clearSymbolValue("core", "PIN_64_FUNCTION_TYPE")
+    Database.clearSymbolValue("core", "PIN_64_PU")
 
-	pinTypes = [{"type":"LED_AH", "mode":"DIGITAL", "dir":"OUT"},
-			{"type":"LED_AL", "mode":"DIGITAL", "dir":"OUT"},
-			{"type":"SWITCH_AH", "mode":"DIGITAL"},
-			{"type":"SWITCH_AL", "mode":"DIGITAL"},
-			{"type":"VBUS_AH", "mode":"DIGITAL", "dir":"OUT"},
-			{"type":"VBUS_AL", "mode":"DIGITAL", "dir":"OUT"}]
+    Database.setSymbolValue("core", "PIN_64_FUNCTION_NAME", "SWITCH")
+    Database.setSymbolValue("core", "PIN_64_FUNCTION_TYPE", "SWITCH_AL")
+    Database.setSymbolValue("core", "PIN_64_PU", "True")
+    
+    BSP_NAME = "sam_e70_xpld"
 
-	execfile(Variables.get("__BSP_DIR") + "/boards/config/bsp_common.py")
+    pinAttributes = [{"attrib":"type", "symbol":"BSP_CUSTOM_TYPE", "label":"Type Name"},
+        {"attrib":"mode", "symbol":"BSP_CUSTOM_MODE", "label":"Mode"},
+        {"attrib":"dir", "symbol":"BSP_CUSTOM_DIR", "label":"Direction"},
+        {"attrib":"lat", "symbol":"BSP_CUSTOM_LAT", "label":"Initial Latch Value"},
+        {"attrib":"od", "symbol":"BSP_CUSTOM_OD", "label":"Open Drain"},
+        {"attrib":"cn", "symbol":"BSP_CUSTOM_CN", "label":"Change Notice"},
+        {"attrib":"pu", "symbol":"BSP_CUSTOM_PU", "label":"Pull Up"},
+        {"attrib":"pd", "symbol":"BSP_CUSTOM_PD", "label":"Pull Down"},
+        {"attrib":"int", "symbol":"BSP_CUSTOM_PIO_INTERRUPT", "label":"PIO Interrupt"}]
+
+    pinTypes = [{"type":"LED_AH", "mode":"DIGITAL", "dir":"OUT"},
+            {"type":"LED_AL", "mode":"DIGITAL", "dir":"OUT"},
+            {"type":"SWITCH_AH", "mode":"DIGITAL"},
+            {"type":"SWITCH_AL", "mode":"DIGITAL"},
+            {"type":"VBUS_AH", "mode":"DIGITAL", "dir":"OUT"},
+            {"type":"VBUS_AL", "mode":"DIGITAL", "dir":"OUT"}]
+
+    execfile(Variables.get("__BSP_DIR") + "/boards/config/bsp_common.py")
