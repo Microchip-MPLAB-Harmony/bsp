cdbb2462b622e4ca0153a3fd08a79d53ea508c53
Arpan <http://support.microchip.com>
2018-09-08 18:55:58 +0530

    [SAME70/SAMV71] updated bsp to generate code even when pin manager is not launched MH- 10106

--- boards/sam_e70_xpld/config/bsp.py
+++ boards/sam_e70_xpld/config/bsp.py
@@ -1,23 +1,17 @@
 def instantiateComponent(bspComponent):
 
     # LED: PC8
-    Database.clearSymbolValue("core", "PIN_82_FUNCTION_NAME")
-    Database.clearSymbolValue("core", "PIN_82_FUNCTION_TYPE")
-    Database.clearSymbolValue("core", "PIN_82_LAT")
-
     Database.setSymbolValue("core", "PIN_82_FUNCTION_NAME", "LED")
     Database.setSymbolValue("core", "PIN_82_FUNCTION_TYPE", "LED_AL")
+    Database.setSymbolValue("core", "PIN_82_DIR", "Out")
     Database.setSymbolValue("core", "PIN_82_LAT", "High")
-    
-   #Switch 1: PA11
-    Database.clearSymbolValue("core", "PIN_64_FUNCTION_NAME")
-    Database.clearSymbolValue("core", "PIN_64_FUNCTION_TYPE")
-    Database.clearSymbolValue("core", "PIN_64_PU")
 
+    #Switch: PA11
     Database.setSymbolValue("core", "PIN_64_FUNCTION_NAME", "SWITCH")
     Database.setSymbolValue("core", "PIN_64_FUNCTION_TYPE", "SWITCH_AL")
     Database.setSymbolValue("core", "PIN_64_PU", "True")
-    
+    Database.setSymbolValue("core", "PIN_64_DIR", "")
+
     BSP_NAME = "sam_e70_xpld"
 
     pinAttributes = [{"attrib":"type", "symbol":"BSP_CUSTOM_TYPE", "label":"Type Name"},
