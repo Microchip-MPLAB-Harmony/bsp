b15ef5f32a716442d0a8da581047aaf07cbeb2b5
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-10-29 10:49:54 +0530

    [BSP] Updated BSP for pic32cm_ls00_curiosity_pro board
    MH3-45497

--- boards/pic32cm_ls00_curiosity_pro/config/bsp.py
+++ boards/pic32cm_ls00_curiosity_pro/config/bsp.py
@@ -24,22 +24,39 @@
 
 def instantiateComponent(bspComponent):
 
-    #LED
+    #LED0
     Database.setSymbolValue("core", "PIN_59_FUNCTION_TYPE", "LED_AL")
-    Database.setSymbolValue("core", "PIN_59_FUNCTION_NAME", "LED")
+    Database.setSymbolValue("core", "PIN_59_FUNCTION_NAME", "LED0")
     Database.setSymbolValue("core", "PIN_59_LAT", "High")
     Database.setSymbolValue("core", "PIN_59_DIR", "Out")
     if Variables.get("__TRUSTZONE_ENABLED") != None and Variables.get("__TRUSTZONE_ENABLED") == "true":
         Database.setSymbolValue("core", "PIN_59_IS_NON_SECURE", 1)
 
-    #Switch
+    #LED1
+    Database.setSymbolValue("core", "PIN_85_FUNCTION_TYPE", "LED_AL")
+    Database.setSymbolValue("core", "PIN_85_FUNCTION_NAME", "LED1")
+    Database.setSymbolValue("core", "PIN_85_LAT", "High")
+    Database.setSymbolValue("core", "PIN_85_DIR", "Out")
+    if Variables.get("__TRUSTZONE_ENABLED") != None and Variables.get("__TRUSTZONE_ENABLED") == "true":
+        Database.setSymbolValue("core", "PIN_85_IS_NON_SECURE", 1)
+
+    #SW0
     Database.setSymbolValue("core", "PIN_60_FUNCTION_TYPE", "SWITCH_AL")
-    Database.setSymbolValue("core", "PIN_60_FUNCTION_NAME", "SWITCH")
+    Database.setSymbolValue("core", "PIN_60_FUNCTION_NAME", "SW0")
     Database.setSymbolValue("core", "PIN_60_PULLEN", "True")
     Database.setSymbolValue("core", "PIN_60_LAT", "High")
     Database.setSymbolValue("core", "PIN_60_INEN", "True")
     if Variables.get("__TRUSTZONE_ENABLED") != None and Variables.get("__TRUSTZONE_ENABLED") == "true":
         Database.setSymbolValue("core", "PIN_60_IS_NON_SECURE", 1)
+
+    #SW1
+    Database.setSymbolValue("core", "PIN_86_FUNCTION_TYPE", "SWITCH_AL")
+    Database.setSymbolValue("core", "PIN_86_FUNCTION_NAME", "SW1")
+    Database.setSymbolValue("core", "PIN_86_PULLEN", "True")
+    Database.setSymbolValue("core", "PIN_86_LAT", "High")
+    Database.setSymbolValue("core", "PIN_86_INEN", "True")
+    if Variables.get("__TRUSTZONE_ENABLED") != None and Variables.get("__TRUSTZONE_ENABLED") == "true":
+        Database.setSymbolValue("core", "PIN_86_IS_NON_SECURE", 1)
 
     BSP_NAME = "pic32cm_ls00_curiosity_pro"
 
